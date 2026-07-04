06d543285591497c6048de5097a5b0e9c5eeec29
Fernando Merlo <fdomerlo@gmail.com>
2026-07-04 18:08:15 -0300

    Update setup_workstation.sh

--- setup_workstation.sh
+++ setup_workstation.sh
@@ -74,8 +74,8 @@ echo -e "Types: deb deb-src\nURIs: http://deb.debian.org/debian\nSuites: trixie-
 sudo apt-get update -qq
 
 # Fasttrack
-sudo apt-get install -y fasttrack-archive-keyring
-echo -e "Types: deb\nURIs: https://fasttrack.debian.net/debian\nSuites: trixie-fasttrack trixie-backports-fasttrack\nComponents: main contrib non-free non-free-firmware\nSigned-By: /usr/share/keyrings/fasttrack-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/fasttrack.sources > /dev/null
+#sudo apt-get install -y fasttrack-archive-keyring
+#echo -e "Types: deb\nURIs: https://fasttrack.debian.net/debian\nSuites: trixie-fasttrack trixie-backports-fasttrack\nComponents: main contrib non-free non-free-firmware\nSigned-By: /usr/share/keyrings/fasttrack-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/fasttrack.sources > /dev/null
 
 # Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
